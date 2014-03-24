39e82d380c6e8965aabd5b14de94d5f8b80f098c
Promoveo <zappeln@gmail.com>
2014-03-23 22:08:15 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HeartApp::Application.config.secret_key_base = 'c3b365a5e610e7c3565662259badc48e6cd1793ea00a50d648bb0297414bbe20db262246a75c1e8d7c67c066eb5663ed16062796737e94a7d59ba8208c63313a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HeartApp::Application.config.session_store :cookie_store, key: '_heart_app_session'
