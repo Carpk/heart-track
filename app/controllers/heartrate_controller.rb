2c3b2f09dbcc4cf1a023d31921afea659cbc1f6b
vinco <zappeln@gmail.com>
2014-04-04 16:26:33 -0500

    submit hr log

--- app/controllers/heartrate_controller.rb
+++ app/controllers/heartrate_controller.rb
@@ -1,2 +1,7 @@
 class HeartrateController < ApplicationController
+  def create
+     # Users.find_by(initials: params[])
+    p params
+    redirect_to user_index_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ HeartApp::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'heartrate#index'
+    root 'heartrate#index'
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+    post 'heartrate' => 'heartrate#create', as: :newlog
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
